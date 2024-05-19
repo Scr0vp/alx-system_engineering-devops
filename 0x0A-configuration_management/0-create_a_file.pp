6957e30e22a05f5283c098d8ec718c3a6b759315
root <hmostaghfir@gmail.com>
2024-05-19 03:29:52 -0700

    CF FILe

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Creates a file name school in /tmp.
+
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
