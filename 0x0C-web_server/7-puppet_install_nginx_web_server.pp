d09a69a2271a37fd66ae1675153884e8efbf37f3
Scr0vp <hmostaghfir@gmail.com>
2024-08-01 10:37:04 -0700

    Puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,33 @@
+# Setup New Ubuntu server with Nginx
+
+# Update system
+exec { 'update_system':
+  command => '/usr/bin/apt-get update',
+}
+
+# Install Nginx package
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['update_system'],
+}
+
+# Create Hello World index.html file
+file { '/var/www/html/index.html':
+  ensure  => 'file',
+  content => 'Hello World!',
+}
+
+# Configure redirection in Nginx default site configuration
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'file',
+  content => template('nginx/default.conf.erb'),  # Use a template for better management
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Service management for Nginx
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx'],
+}
